b9922c551f4b895ab2906bd65549e7a7798e9f65
vickyvk326 <vigneshravi326@gmail.com>
2026-07-09 19:29:40 +0530

    added more

--- app/new-expense.tsx
+++ app/new-expense.tsx
@@ -1,16 +1,28 @@
 import PageView from '@/components/PageView';
 import { ThemedIonicons } from '@/components/ThemedIonicons';
 import { Input } from '@/components/ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 import { currencyCodeMap, TRANSACTION_CATEGORIES } from '@/constants';
 import { useSettingsStore } from '@/store/useSettingsStore';
+import { TriggerRef } from '@rn-primitives/select';
 import { useRouter } from 'expo-router';
 import React, { useState } from 'react';
-import { Pressable, ScrollView, Text, View } from 'react-native';
+import { Platform, Pressable, ScrollView, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const NewExpense = () => {
   const router = useRouter();
-  const [amount, setAmount] = useState('0');
+  const [amount, setAmount] = useState('');
   const [category, setCategory] = useState<string>(TRANSACTION_CATEGORIES[0]);
+  const [note, setNote] = useState('');
   function handleAmountChange(text: string) {
     const cleaned = text.replace(/[^0-9.]/g, '');
     const parts = cleaned.split('.');
@@ -21,9 +33,21 @@ const NewExpense = () => {
   }
   const { settings } = useSettingsStore();
   const { currency } = settings.systemSettings;
+  const selectedCategoryOption = {
+    label: category,
+    value: category,
+  };
+  const ref = React.useRef<TriggerRef>(null);
+  const insets = useSafeAreaInsets();
+  const contentInsets = {
+    top: insets.top,
+    bottom: Platform.select({ ios: insets.bottom, android: insets.bottom + 24 }),
+    left: 12,
+    right: 12,
+  };
   return (
     <PageView title="New Expense">
-      <ScrollView className="flex-1">
+      <View className="flex-1">
         {/* Header */}
         <View className="flex flex-row items-center gap-5 bg-background p-5 mb-5">
           <Pressable onPress={() => router.back()}>
@@ -39,14 +63,14 @@ const NewExpense = () => {
 
         {/* Amount */}
         <View className="flex flex-col gap-1 items-center justify-center py-5">
-          <Text className="text-muted-foreground">Amount</Text>
+          <Text className="text-muted-foreground font-medium">Amount</Text>
           <View className="flex flex-row items-center justify-center">
             <Text className="text-4xl font-bold text-secondary-foreground mt-1">
               {currencyCodeMap[currency]}
             </Text>
             <Input
               keyboardType="decimal-pad"
-              className="border-0 w-min text-4xl font-bold h-min"
+              className="border-0 w-min text-4xl font-bold h-min p-0"
               placeholder="0"
               style={{
                 backgroundColor: 'transparent',
@@ -60,59 +84,73 @@ const NewExpense = () => {
           </View>
         </View>
 
-        <View className="flex-1 bg-background mt-5 p-5">
+        <ScrollView className="flex-1" contentContainerClassName="flex-1">
           {/* Category */}
-          <Text className="text-muted text-md font-medium">Category</Text>
-          <View className="flex flex-row items-center gap-3 mt-3" style={{ flexWrap: 'wrap' }}>
-            {TRANSACTION_CATEGORIES.map((c) => (
-              <Pressable
-                key={c}
-                className={`rounded-xl px-2 py-1.5 ${c === category ? 'bg-accent' : 'bg-muted'}`}
-                onPress={() => setCategory(c)}
-              >
-                <Text
-                  className={`text-secondary-foreground text-sm font-medium ${c === category ? 'text-white' : 'text-muted-foreground'}`}
-                >
-                  {c[0].toUpperCase()}
-                  {c.slice(1)}
-                </Text>
-              </Pressable>
-            ))}
+          <View className="flex-row items-center bg-background mt-5 py-3 px-5">
+            <Text className="text-muted-foreground text-base font-medium flex-1 px-3">
+              Category
+            </Text>
+            <Select
+              defaultValue={selectedCategoryOption}
+              value={selectedCategoryOption}
+              onValueChange={(option) => setCategory(option?.value as string)}
+            >
+              <SelectTrigger ref={ref} className="w-min border-0 text-base">
+                <SelectValue className="text-base font-medium" placeholder="USD $" />
+              </SelectTrigger>
+              <SelectContent insets={contentInsets} className="w-[180px] text-base">
+                <ScrollView style={{ maxHeight: 500 }}>
+                  <SelectGroup className="text-base">
+                    <SelectLabel>Currency</SelectLabel>
+                    {TRANSACTION_CATEGORIES.map((category) => (
+                      <SelectItem
+                        key={category}
+                        label={`${category[0].toUpperCase()}${category.slice(1)}`}
+                        value={category}
+                        className="text-base font-medium"
+                      >
+                        {category[0].toUpperCase()}
+                        {category.slice(1)}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </ScrollView>
+              </SelectContent>
+            </Select>
           </View>
-        </View>
 
-        {/* Details */}
-        <View className="bg-background py-4 px-5">
-          <View className="flex flex-col">
-            <React.Fragment>
+          {/* Details */}
+          <View className="flex-1 bg-background px-5">
+            <View className="flex flex-col">
+              <View className="h-[1px] bg-secondary-foreground/10" />
               <View className="p-3 flex flex-row items-center justify-between gap-5">
-                <Text className="flex-1 text-secondary-foreground text-base font-medium">Date</Text>
-                <Text>Today, Jul 7</Text>
-              </View>
-            </React.Fragment>
-
-            <View className="h-[1px] bg-secondary-foreground/10" />
-
-            <React.Fragment>
-              <View className="p-3 flex flex-row items-center justify-between gap-5">
-                <Text className="flex-1 text-secondary-foreground text-base font-medium">
-                  Account
+                <Text className="flex-1 text-muted-foreground text-base font-medium">Date</Text>
+                <Text className="text-secondary-foreground text-base font-medium">
+                  Today, Jul 7
                 </Text>
-                <Text>Debit ••4821</Text>
               </View>
-            </React.Fragment>
 
-            <View className="h-[1px] bg-secondary-foreground/10" />
-
-            <React.Fragment>
-              <View className="p-3 flex flex-col gap-2">
-                <Text className="flex-1 text-secondary-foreground text-base font-medium">Note</Text>
-                <Text>Lunch with team</Text>
+              <View className="h-[1px] bg-secondary-foreground/10" />
+              <View className="p-3 flex flex-row items-center justify-between gap-5">
+                <Text className="flex-1 text-muted-foreground text-base font-medium">Account</Text>
+                <Text className="text-secondary-foreground text-base font-medium">
+                  Debit ••4821
+                </Text>
               </View>
-            </React.Fragment>
+
+              <View className="h-[1px] bg-secondary-foreground/10" />
+              <Text className="p-3 text-muted-foreground text-base font-medium">Note</Text>
+              <View className="px-3 flex flex-col gap-2">
+                <Input
+                  className="text-secondary-foreground text-base font-medium"
+                  value={note}
+                  onChangeText={(t) => setNote(t)}
+                />
+              </View>
+            </View>
           </View>
-        </View>
-      </ScrollView>
+        </ScrollView>
+      </View>
     </PageView>
   );
 };
